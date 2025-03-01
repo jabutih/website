ebf865e7894efa3c9d4985d840dcb84a0e3ab4f2
Jabuti <guilhermeoliveirfm@gmail.com>
2025-03-01 13:59:35 -0300

    webstorm

---
+++ script.js
@@ -0,0 +1,13 @@
+var typed = new Typed(".auto-type", {
+    strings: [
+        "いろはにほへと<br>ちりぬるを<br>わかよたれそ<br>つねならむ<br>うゐのおくやま<br> けふこえて<br>あさきゆめみし<br>ゑひもせす<br><span class='author'>弘法大師</span>",
+        "この美しい短歌こそ日本人の心<br>敷島の<br>大和心を<br>人問わば<br>朝日ににおう<br>山桜花<br><span class='author'>山部赤人</span>",
+        "体育館<br>虫が入ったら<br>殺します<br><span class='author'>さっちゃん</span>",
+        "夏山の<br>木末の茂に<br>ほととぎす<br>鳴きとよむなる<br>声の遥けさ<br> <span class='author'>柿本人麻呂</span>",
+        "天の海に<br>雲の波立ち<br>月の船<br>星の林に<br>漕ぎ隠る見ゆ<br><span class='author'>在原業平</span>",
+        "秋山に<br>もみつ木の葉の<br>うつりなば<br>さらにや秋を<br>見まく欲りせむ<br><span class='author'>藤原定家</span>",
+        "天の下<br>すでに覆ひて<br>ふる雪の<br>光を見れば<br>貴くもあるか<br><span class='author'>小野小町</span>"],
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true
+})

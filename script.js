9dc77d1f6090767fe8bf875c2d87b1506e1eb68b
Jabuti <guilhermeoliveirfm@gmail.com>
2025-03-06 17:55:46 -0300

    atualização

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var typed = new Typed(".auto-type", {
         "天の海に<br>雲の波立ち<br>月の船<br>星の林に<br>漕ぎ隠る見ゆ<br><span class='author'>在原業平</span>",
         "秋山に<br>もみつ木の葉の<br>うつりなば<br>さらにや秋を<br>見まく欲りせむ<br><span class='author'>藤原定家</span>",
         "天の下<br>すでに覆ひて<br>ふる雪の<br>光を見れば<br>貴くもあるか<br><span class='author'>小野小町</span>"],
-    typeSpeed: 50,
+    typeSpeed: 40,
     backSpeed: 30,
     backDelay: 5000,
     loop: true,
